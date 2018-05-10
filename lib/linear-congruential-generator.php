6dd63a697e89c714dae7871b8af77ab921be0a4c
knowgod <arkuzhel@gmail.com>
2018-05-10 10:54:35 +0300

    Add comments and link to PRNG used
      * Add argument types
      * PRNG - pseudo random number generator

--- lib/linear-congruential-generator.php
+++ lib/linear-congruential-generator.php
@@ -1,12 +1,27 @@
 <?php
-function bsd_rand($seed)
+/**
+ * These are implementations of LCG algorithm for BSD and MS systems.
+ * @see http://rosettacode.org/wiki/Linear_congruential_generator#PHP
+ */
+
+/**
+ * @param $seed
+ *
+ * @return Closure
+ */
+function bsd_rand(int $seed)
 {
     return function () use (&$seed) {
         return $seed = (1103515245 * $seed + 12345) % (1 << 31);
     };
 }
 
-function msvcrt_rand($seed)
+/**
+ * @param $seed
+ *
+ * @return Closure
+ */
+function msvcrt_rand(int $seed)
 {
     return function () use (&$seed) {
         return ($seed = (214013 * $seed + 2531011) % (1 << 31)) >> 16;
