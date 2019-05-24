8b0911a9d3694f826ce12139ca3805dce432692d
knowgod <arkadij@madepeople.se>
2019-05-24 15:45:30 +0300

    Re-format card suits nad faces
     * Move card suits, colours, and faces to the separate Interface

--- src/Card.php
+++ src/Card.php
@@ -10,40 +10,9 @@ namespace Freecell;
  * Class CardRepresentation
  *
  */
-class Card implements Api\GameObjectInterface
+class Card implements Api\GameObjectInterface, Api\Object\CardInterface
 {
     const NOT_SET = '';
-
-    const COLOUR_RED   = 1;
-    const COLOUR_BLACK = 0;
-
-    const SUIT_CLUB    = 0;
-    const SUIT_DIAMOND = 1;
-    const SUIT_HEART   = 2;
-    const SUIT_SPADES  = 3;
-
-    private $suitFaces = [
-        self::SUIT_CLUB    => "\u{2663}",
-        self::SUIT_DIAMOND => "\u{2666}",
-        self::SUIT_HEART   => "\u{2665}",
-        self::SUIT_SPADES  => "\u{2660}",
-    ];
-
-    private $cardFaces = [
-        0  => ' A ',
-        1  => ' 2 ',
-        2  => ' 3 ',
-        3  => ' 4 ',
-        4  => ' 5 ',
-        5  => ' 6 ',
-        6  => ' 7 ',
-        7  => ' 8 ',
-        8  => ' 9 ',
-        9  => ' 10',
-        10 => ' J ',
-        11 => ' Q ',
-        12 => ' K ',
-    ];
 
     /**
      * @var int
@@ -129,7 +98,7 @@ class Card implements Api\GameObjectInterface
     {
         return ($this->isInit())
             ? self::NOT_SET
-            : $this->suitFaces[ $this->suit ] . $this->cardFaces[ $this->value ];
+            : self::FACE_SUIT[$this->suit] . self::FACE_CARD[$this->value];
     }
 
     /**
