ca5cb6b7df4f6c81e04e1b9d0408d72b1e734784
knowgod <arkadij@madepeople.se>
2019-08-11 03:26:33 +0300

    [WIP] Introduce Stack entity
     * Subtype of Column which cards are always sorted
     * Test Stack creation

---
+++ src/Deck/Column/Stack.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © Madepeople, Inc. All rights reserved.
+ *
+ * @author    Arkadij Kuzhel <arkadij@madepeople.se>
+ * @created   11.08.19
+ */
+declare(strict_types=1);
+
+namespace Freecell\Deck\Column;
+
+use Freecell\Card;
+use Freecell\CardFactory;
+use Freecell\Deck\Column;
+use Freecell\Deck\ColumnFactory;
+
+/**
+ * Class Stack
+ * Implementation of Column that is always sorted
+ *
+ */
+class Stack extends Column
+{
+    /** @noinspection MagicMethodsValidityInspection */
+    /** @noinspection PhpMissingParentConstructorInspection */
+    public function __construct(ColumnFactory $columnFactory, array $cards = [])
+    {
+        $this->cardFactory = new CardFactory();
+
+        foreach ($cards as $card) {
+            if (is_int($card)) {
+                $card = $this->cardFactory->create($card);
+            }
+            if ($card instanceof Card) {
+                if (!$this->canPlace($card)) {
+                    throw new \InvalidArgumentException('Error creating stack: The set of cards is not stackable.');
+                }
+                $this->addCard($card);
+            }
+        }
+    }
+}
